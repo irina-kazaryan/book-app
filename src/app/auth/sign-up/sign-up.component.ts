1421b8cd881ddf7d66d796cf7e031f15e6a2b1a1
irina-kazaryan <92260975+irina-kazaryan@users.noreply.github.com>
2021-12-17 22:59:55 +0400

    Update sign-up.component.ts

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { from } from 'rxjs';
 import { finalize } from 'rxjs/operators';
@@ -22,9 +23,13 @@ export class SignUpComponent implements OnInit {
     private loadingService: LoadingService
   ) {}
 
-  signUp({ email, password }: SignUpForm) {
+  signUp(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
     this.loadingService.start();
-    from(this.auth.signUp({ email, password }))
+    from(this.auth.signUp(form.value))
       .pipe(finalize(() => this.loadingService.stop()))
       .subscribe(() => this.router.navigate(['content']));
   }
